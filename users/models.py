3d89238edaee232ff1cc16d36f048ba1214a21a6
suxrobshukurov <sdead96@gmail.com>
2021-01-18 13:26:30 +0300

    reformat pep8

--- users/models.py
+++ users/models.py
@@ -10,9 +10,16 @@ from django.urls import reverse
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     img = models.ImageField(default='default.jpg', upload_to='user_images')
-    first_name = models.CharField(verbose_name='Имя', max_length=150, null=True)
-    last_name = models.CharField(verbose_name='Фамилия', max_length=150, null=True)
-    father_name = models.CharField(verbose_name='Отчество', max_length=150, null=True)
+    first_name = models.CharField(
+        verbose_name='Имя', max_length=150, null=True)
+    last_name = models.CharField(
+        verbose_name='Фамилия',
+        max_length=150,
+        null=True)
+    father_name = models.CharField(
+        verbose_name='Отчество',
+        max_length=150,
+        null=True)
     bio = models.TextField(verbose_name='О себе', max_length=4000, null=True)
 
     def __str__(self):
@@ -31,6 +38,7 @@ class Profile(models.Model):
             image.thumbnail(resize)
             image.save(self.img.path)
 
+
 class UsersList(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
 
@@ -39,6 +47,7 @@ class UsersList(models.Model):
 def create_profile(sender, instance, created, **kwards):
     if created:
         Profile.objects.create(user=instance)
+
 
 @receiver(post_save, sender=User)
 def save_profile(sender, instance, **kwards):
